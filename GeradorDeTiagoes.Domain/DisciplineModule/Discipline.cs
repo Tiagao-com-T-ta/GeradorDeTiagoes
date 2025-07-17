36151bfd5d04017815d113b6d934c1e8f6b75d19
danilo <danilocencigg@gmail.com>
2025-07-17 14:54:01 -0300

    added domain and structure

---
+++ GeradorDeTiagoes.Domain/DisciplineModule/Discipline.cs
@@ -0,0 +1,26 @@
+﻿using GeradorDeTiagoes.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeTiagoes.Domain.DisciplineModule
+{
+    public class Discipline : BaseEntity<Discipline>
+    {
+        public string Name { get; set; }
+
+        public Discipline() { }
+
+        public Discipline(string name)
+        {
+            Name = name;
+        }   
+
+        public override void Update(Discipline EditedEntity)
+        {
+            Name = EditedEntity.Name;
+        }
+    }
+}
---
+++ GeradorDeTiagoes.Structure.Files/DisciplineModule/DisciplineRepositoryFile.cs
@@ -0,0 +1,20 @@
+﻿using GeradorDeTiagoes.Domain.DisciplineModule;
+using GeradorDeTiagoes.Structure.Files.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeTiagoes.Structure.Files.DisciplineModule
+{
+    public class DisciplineRepositoryFile : BaseRepositoryFile<Discipline>, IDisciplineRepository
+    {
+        public DisciplineRepositoryFile(DataContext dataContext) : base(dataContext) { }
+
+        protected override List<Discipline> GetRegisters()
+        {
+            return dataContext.Disciplines;
+        }
+    }
+}
