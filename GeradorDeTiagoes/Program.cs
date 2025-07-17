841404ad6b6500460b69892ea9319cd86fa3f881
davidinovargas <davidinovargas@gmail.com>
2025-07-17 16:08:59 -0300

    Added functional views for Subject

--- GeradorDeTiagoes/Program.cs
+++ GeradorDeTiagoes/Program.cs
@@ -1,3 +1,11 @@
+using GeradorDeTiagoes.Domain.DisciplineModule;
+using GeradorDeTiagoes.Domain.Entities;
+using GeradorDeTiagoes.Domain.Shared;
+using GeradorDeTiagoes.Domain.SubjectsModule;
+using GeradorDeTiagoes.Structure.Files.DisciplineModule;
+using GeradorDeTiagoes.Structure.Files.Shared;
+using GeradorDeTiagoes.Structure.Files.SubjectsModule;
+
 namespace GeradorDeTiagoes
 {
     public class Program
@@ -5,9 +13,13 @@ namespace GeradorDeTiagoes
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddScoped<ISubjectRepository, SubjectRepositoryFile>();
+            builder.Services.AddScoped<IRepository<Subject>, SubjectRepositoryFile>();
+            builder.Services.AddScoped<IDisciplineRepository, DisciplineRepositoryFile>();
+            builder.Services.AddScoped<IRepository<Discipline>, DisciplineRepositoryFile>();
+            builder.Services.AddScoped<DataContext>();
             var app = builder.Build();
 
             app.UseAntiforgery();
